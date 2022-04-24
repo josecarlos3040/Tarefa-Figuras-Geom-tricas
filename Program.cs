88c5b80b05297a50c34c0ca89b6a5df954f0b149
José Carlos <102222057+josecarlos3040@users.noreply.github.com>
2022-04-24 18:07:20 -0300

    Figuras-Geometricas

---
+++ Program.cs
@@ -0,0 +1,21 @@
+Quadrado q1 = new Quadrado(4.0);
+Console.WriteLine(q1.Lado);
+q1.Lado = 5.0;
+Console.WriteLine(q1.Lado);
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+Console.WriteLine("\n");
+
+Quadrado q2 = new Quadrado(10.0);
+Console.WriteLine(q2.Lado);
+Console.WriteLine(q2.Area);
+Console.WriteLine(q2.Perimetro);
+
+Console.WriteLine("\n");
+
+Retangulo r1 = new Retangulo(2.0, 5.0);
+Console.WriteLine(r1.Base);
+Console.WriteLine(r1.Altura);
+Console.WriteLine(r1.Area);
+Console.WriteLine(r1.Perimetro);
---
+++ Retangulo.cs
@@ -0,0 +1,12 @@
+public class Retangulo
+{
+    public double Base {get; set;}
+    public double Altura {get; set;}
+    public double Area { get => Base * Altura;}
+    public double Perimetro { get => (2*Base) + (2*Altura);}
+    public Retangulo (double _base, double altura)
+    {
+        Base = _base;
+        Altura = altura;
+    }
+}
